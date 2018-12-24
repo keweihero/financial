dfb2ff71e4ba98e7486334ede2ca6dd1f4b6222b
kewei_liao <kewei_liao@163.com>
2018-12-24 23:16:13 +0800

    管理端:创建产品/查询单个产品/多条件分页查询产品

---
+++ manager/src/main/java/com/powderblue/manager/ManagerApp.java
@@ -0,0 +1,18 @@
+package com.powderblue.manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+
+/**
+ * @author powderblue
+ * 管理端启动类
+ *
+ */
+@SpringBootApplication
+@EntityScan(basePackages = {"com.powderblue.entity"})
+public class ManagerApp {
+    public static void main(String[] args) {
+        SpringApplication.run(ManagerApp.class);
+    }
+}
---
+++ manager/src/main/java/com/powderblue/manager/repositories/ProductRepository.java
@@ -0,0 +1,8 @@
+package com.powderblue.manager.repositories;
+
+import com.powderblue.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+
+public interface ProductRepository extends JpaRepository<Product, String>, JpaSpecificationExecutor<Product> {
+}
